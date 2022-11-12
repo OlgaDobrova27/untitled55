3b9af85888412e540d6a608a693258ab3fc5cf3e
olechka27 <olechka.dobrova@gmail.com>
2022-11-12 15:23:21 +0500

    орпо

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import Product.Product;
+import Recipe.Good;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static <ShoppingList> void main(String[] args) {
+       }
+        public class ShoppingList {
+
+        ShoppingList shoppingList = new ShoppingList();
+        Set<Product> set;
+
+            public ShoppingList() {
+
+            }
+
+            {
+            set = new HashSet<>();
+        }
+
+        public void add(Good good, double weight, double price) {
+            final Product product = new Product(good, weight, price);
+            if (set.contains(product)) {
+                throw new IllegalArgumentException("Продукт есть в списке!");
+            }
+            set.add(product);
+        }
+
+        public void markAsPurchased (Good good){
+            set.stream().filter(p -> p.getGood() == good).findAny().ifPresent(p -> p.setPurchased(true));
+        }
+
+        public void remove (Good good){
+            final Product target;
+            target = set.stream().filter(p -> p.getGood() == good).findAny().orElse(null);
+            set.remove(target);
+        }
+
+        public void show () {
+            set.forEach(p -> {
+                System.out.printf("%s, %.2f кг, %.2f рублей, купленный - %b\n",
+                        p.getGood().getName(),
+                        p.getWeight(),
+                        p.getPrice(),
+                        p.isPurchased());
+            });
+        }
+    }
+
+    private static void ShoppingList() {
     }
 }
---
+++ src/Recipe/Recipe.java
@@ -0,0 +1,69 @@
+package Recipe;
+
+import Product.Product;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Recipe {
+
+    private String name;
+    private double totalPrice;
+    private Set<Product> products = new HashSet<>();
+
+    public Recipe(String name) {
+        setName(name);
+        totalPrice = 0;
+    }
+
+    public void addProduct(Product product) {
+        if (products.contains(product)) {
+            throw new IllegalArgumentException("Эти продукты уже есть в списке!");
+        }
+        products.add(product);
+        totalPrice += product.getPrice();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Неверное название рецепта!");
+        }
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Recipe recipe = (Recipe) o;
+
+        if (Double.compare(recipe.totalPrice, totalPrice) != 0) return false;
+        if (!Objects.equals(name, recipe.name)) return false;
+        return Objects.equals(products, recipe.products);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(totalPrice);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (products != null ? products.hashCode() : 0);
+        return result;
+    }
+}
